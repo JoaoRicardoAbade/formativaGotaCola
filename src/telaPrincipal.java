24a3da1265be616af1639d58f8899825368db97e
JoaoRicardoAbade <joaoricardoabade@gmail.com>
2024-05-21 10:18:15 -0300

    tela principal antes das outras funções

---
+++ src/telaPrincipal.java
@@ -0,0 +1,48 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class telaPrincipal extends JFrame{
+    private JLabel megaSena;
+    private JButton gerarJogos;
+    private JButton sorteioDaMega;
+    private JButton LerJogos;
+    private JButton Sair;
+    private JPanel painelPrincipal;
+
+    public telaPrincipal()throws HeadlessException {
+        setSize(900,600);
+        setContentPane(painelPrincipal);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+
+        LerJogos.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                telaLerJogos i = new telaLerJogos();
+            }
+        });
+
+        gerarJogos.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                telaGerarJogos p= new telaGerarJogos();
+            }
+        });
+
+        sorteioDaMega.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                telaSortearJogos a= new telaSortearJogos();
+            }
+        });
+
+        Sair.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+}
---
+++ src/telaSortearJogos.java
@@ -0,0 +1,5 @@
+import javax.swing.*;
+
+public class telaSortearJogos {
+    private JPanel painelSorteio;
+}
